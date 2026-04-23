37c86d9d0ee308622cb7a18b7252d553e48b5c0a
ram-dev95 <hasmagomed1111@gmail.com>
2026-04-23 17:38:29 +0300

    Создал модели для жанров и отзывов, сделал слои для бизнес-логики и транспорта.

---
+++ internal/models/reviews.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Review struct {
+	gorm.Model
+	Rating  int    `json:"rating"`
+	Comment string `json:"comment"`
+	MovieID uint   `json:"movie_id" gorm:"not null;index"`
+}
+type CreateReviewRequest struct {
+	Rating  *int    `json:"rating"`
+	Comment *string `json:"comment"`
+	MovieID *uint   `json:"movie_id"`
+}
+type UpdateReviewRequest struct {
+	Reting  *int    `json:"rating"`
+	Comment *string `json:"comment"`
+	MovieID *uint   `json:"movie_id"`
+}
