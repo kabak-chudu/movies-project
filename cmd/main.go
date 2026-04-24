6d974fd0ffce5679c83ba8b77344130a73d9db6b
kabak-chudu <islambataev2009is@mail.ru>
2026-04-24 11:09:31 +0300

    logger added

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
+	"log/slog"
 	"movies/internal/config"
 	"movies/internal/models"
 	"movies/internal/repository"
 	"movies/internal/services"
 	"movies/internal/transport"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+
 	db := config.SetDatabaseConnection()
+
+	env := os.Getenv("ENV")
 
 	if err := db.AutoMigrate(&models.Movie{}); err != nil {
 		panic(err)
 	}
 
-	movieRepo := repository.NewMovieRepository(db)
+	movieRepo := repository.NewMovieRepository(db, logger)
 
-	movieService := services.NewMovieService(movieRepo)
+	movieService := services.NewMovieService(movieRepo, logger)
 
 	router := gin.Default()
-	transport.RegisterRoutes(router, movieService)
-	if err := router.Run(":8080"); err != nil {
+	transport.RegisterRoutes(router, movieService, logger)
+
+	port := ":8080"
+	logger.Info("server started",
+		"addr=", port,
+		"env=", env,
+	)
+	if err := router.Run(port); err != nil {
 		panic(err)
 	}
+
 }
