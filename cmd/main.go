70a0a4131d371029aac41cd98ddcb3fa1fd47614
Veoler <iiterhoev@gmail.com>
2026-04-28 15:27:29 +0300

    Transport: Подготовил хендлеры, добавил нужные правки в main и routers

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"movies/internal/services"
 	"movies/internal/transport"
 	"os"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,16 +20,25 @@ func main() {
 
 	env := os.Getenv("ENV")
 
-	if err := db.AutoMigrate(&models.Movie{}); err != nil {
+	if err := db.AutoMigrate(&models.User{}, &models.Movie{}, &models.Collection{}); err != nil {
 		panic(err)
 	}
 
+	var count int64
+	db.Model(&models.User{}).Count(&count)
+	if count == 0 {
+    	db.Create(&models.User{Username: "test_user"})
+    	fmt.Println("тестовый пользователь создан (ID: 1)")
+	}
+
 	movieRepo := repository.NewMovieRepository(db, logger)
+	collectionRepo := repository.NewCollectionRepository(db)
 
 	movieService := services.NewMovieService(movieRepo, logger)
+	collectionService := services.NewCollectionService(collectionRepo, movieRepo)
 
 	router := gin.Default()
-	transport.RegisterRoutes(router, movieService, logger)
+	transport.RegisterRoutes(router, movieService, collectionService, logger)
 
 	port := ":8080"
 	logger.Info("server started",
--- internal/transport/collection_handler.go
+++ internal/transport/collection_handler.go
@@ -1,27 +1,133 @@
 package transport
 
 import (
+	"movies/internal/models"
+	"movies/internal/services"
+	"net/http"
+	"errors"
+	"strconv"
 	"github.com/gin-gonic/gin"
-	"movies/internal/services"
-	"movies/internal/repository"
-	"movies/internal/services"
 )
 
 type CollectionHandler struct {
-	service services.CollectionService
+	collection services.CollectionService
 }
 
-func NewCollectionHandler(service services.CollectionService) *CollectionHandler {
-	return &CollectionHandler{service: service}
+func NewCollectionHandler(collection services.CollectionService) *CollectionHandler {
+	return &CollectionHandler{collection: collection}
 }
 
-func (h *CollectionHandler) RegisterRoutes(r *gin.Engine) {
-	collections := r.Collection("/collections")
+func (h *CollectionHandler) RegisterRoutes(router *gin.Engine) {
+	collections := router.Group("/collections")
 	{
-		collection.Get("", h.GetAll)
-		collection.Get("", h.Create)
-		collection.Get("/:id", h.GetByID)
-		collection.Get("/:id/movies", h.AddMovie)
-		collection.Get("/:id/movies/:movie_id", h.RemoveMovie)
+		collections.GET("", h.GetAll)
+		collections.POST("", h.Create)
+		collections.GET("/:id", h.GetByID)
+		collections.POST("/:id/movies", h.AddMovie)
+		collections.DELETE("/:id/movies/:movie_id", h.RemoveMovie)
 	}
+}
+
+func (h *CollectionHandler) Create(c *gin.Context) {
+	var req models.CollectionCreateRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	collection, err := h.collection.CreateCollection(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, collection)
+}
+
+func (h *CollectionHandler) GetByID(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный ID коллекции"})
+		return
+	}
+	
+	collection, err := h.collection.GetCollectionByID(uint(id))
+	if err != nil {
+		if errors.Is(err, services.ErrCollectionNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		} else {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        }
+        return
+    }
+
+	c.JSON(http.StatusOK, collection)
+}
+
+func (h *CollectionHandler) GetAll(c *gin.Context) {
+	collection, err := h.collection.GetAllCollections()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, collection)
+}
+
+func (h *CollectionHandler) AddMovie(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный ID коллекции"})
+		return
+	}
+	
+	var req models.CollectionAddRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	collection, err := h.collection.AddMovieToCollection(uint(id), req); if err != nil {
+		switch {
+        	case errors.Is(err, services.ErrCollectionNotFound):
+        	    c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        	case errors.Is(err, services.ErrMovieNotFound):
+        	    c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        	default:
+        	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    	}
+        return
+	}
+
+	c.JSON(http.StatusOK, collection)
+}
+
+func (h *CollectionHandler) RemoveMovie(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный ID коллекции"})
+		return
+	}
+
+	mID, err := strconv.ParseUint(c.Param("movie_id"), 10, 64)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Некорректный ID фильма"})
+        return
+    }
+
+	if err := h.collection.RemoveMovieFromCollection(uint(id), uint(mID)); err != nil {
+		switch {
+        	case errors.Is(err, services.ErrCollectionNotFound):
+        	    c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        	case errors.Is(err, services.ErrMovieNotFound):
+        	    c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        	default:
+        	    c.JSON(http.StatusInternalServerError, gin.H{"error": "Ошибка сервера"})
+        }
+        return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "deleted"})
 }
