4b6bfb960de9bc8cdc4c44cb1e7b6b7e167c7d3e
kabak-chudu <islambataev2009is@mail.ru>
2026-04-30 15:08:31 +0300

    added login repository

---
+++ internal/repository/login_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"movies/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type LoginRepository interface {
+	Login(*models.Login) error
+}
+
+type gormLoginRepository struct {
+	db *gorm.DB
+}
+
+func NewLoginRepository(
+	db *gorm.DB,
+) LoginRepository {
+	return &gormLoginRepository{db: db}
+}
+
+func (r *gormLoginRepository) Login(user *models.Login) error {
+	if user == nil {
+		return nil
+	}
+	if err := r.check(user); err != nil {
+		return err
+	}
+
+	return r.db.Create(&user).Error
+}
+
+func (r *gormLoginRepository) check(req *models.Login) error {
+	var user models.User
+
+	err := r.db.Where("username = ? AND password = ?", req.Username, req.Password).First(&user).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
